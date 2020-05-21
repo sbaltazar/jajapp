8805cb885ce564c46ba0f1db03efa4ad08835c98
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-05-21 17:48:49 -0400

    feat: Added java library dependency
    A new java lubrary was created to provide a simple 'joke' to be used by the
    activity android application

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.teller.jajava.JajaTeller;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, JajaTeller.tellJajavaJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jajava/src/main/java/com/teller/jajava/JajaTeller.java
@@ -0,0 +1,8 @@
+package com.teller.jajava;
+
+public class JajaTeller {
+
+    public static String tellJajavaJoke(){
+        return "Knock, Knock, is jajava!";
+    }
+}
